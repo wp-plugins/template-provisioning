9d1d3ad35b15bc6762dac8e6ab74ee2e63d0a0fe
jasontremblay <jasontremblay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-18 13:38:03 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/template-provisioning/trunk@136341 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/template-js.php
@@ -0,0 +1,4 @@
+<!-- JAVASCRIPT PROVISIONING (searching for 'js/global.js', 'js/<?php echo $template_basename; ?>.js') -->
+<?php if ($js_src_global) : ?><script type="text/javascript" src="<?php echo $js_src_global; ?>"></script><?php endif; ?>
+<?php if ($js_src_template) : ?><script type="text/javascript" src="<?php echo $js_src_template; ?>"></script><?php endif; ?>
+
