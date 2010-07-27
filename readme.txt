e6ebf962ba193a06ebe777ce9d78bba34fe1e23a
jasontremblay <jasontremblay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-27 04:50:58 +0000

    Tagging version 0.2.3
    git-svn-id: https://plugins.svn.wordpress.org/template-provisioning/tags/0.2.3@268008 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.bigbigtech.com/
 Tags: template, theme, css, javascript
 Requires at least: 2.8
 Tested up to: 3.0
-Stable tag: 0.2.2
+Stable tag: 0.2.3
 
 The Template Provisioning plugin automatically links each blog page to .css and .js files that correspond to its template.
 
@@ -33,7 +33,7 @@ Will this plugin work with your theme? Probably. It's completely additive, and d
 
 = Usage =
 
-Using the plugin is easy.  Just create some .css and .js files where the plugin expects them... in the same directory as your template files.  There are a series of files that it looks for when rendering a page using any given template file:
+Using the plugin is easy.  Just create some .css, .less and .js files where the plugin expects them... in the same directory as your template files.  There are a series of files that it looks for when rendering a page using any given template file:
 
 Stylesheets for "&lt;template\_name&gt;.php":
 included in page &lt;head&gt; by wp\_head() function
@@ -55,14 +55,40 @@ included near the &lt;/body&gt; tag by wp\_footer() function
 * js/global.footer.js
 * js/&lt;template\_name&gt;.footer.js
 
+If your script or stylesheet depends on others (i.e. jQuery) being loaded first, simply enqueue them in your template header before the call to wp_head().
+
+You can also specify dependencies in comments in included scripts and stylesheets using the following syntax (dependencies should be comma-separated):
+* // NEEDS: jquery, jquery-cycle
+
+NOTE: the above syntax will not actually enqueue the dependencies... it will only require them for our included scripts.
+
+= less.js support =
+
+As of version 0.2.3, the plugin will also look for .less files
+
+* css/global.less
+* css/ie/global.less
+* css/&lt;template\_name&gt;.less
+* css/ie/&lt;template\_name&gt;.less
+
+If you have the "less.js" javascript file in the expected location, it will also be enqueued:
+
+* js/less.js
+
 == Frequently Asked Questions ==
 
 None yet... post your questions to the [plugin homepage](http://www.bigbigtech.com/wordpress-plugins/template-provisioning "Template Provisioning Homepage")
 
 == Changelog ==
 
+= 0.2.3 =
+
+* Added support for less.js
+* Scripts/stylesheets can now specify their own dependencies
+
 = 0.2.2 =
 
+* Removed file extension from enqueue handles
 * Changed "Required at least" back to 2.8
 * Don't enqueue resources on admin pages
 
--- template-provisioning.php
+++ template-provisioning.php
@@ -3,7 +3,7 @@
 Plugin Name: Template Provisioning
 Plugin URI: http://www.bigbigtech.com/wordpress-plugins/template-provisioning/
 Description: Automatically links to css and js files for the current template file.
-Version: 0.2.2
+Version: 0.2.3
 Author: Jason Tremblay
 Author URI: http://www.alertmybanjos.com
 */
@@ -45,6 +45,9 @@ class Template_Provisioning
 		// TAKING ADVANTAGE OF OTHER COOL TEMPLATE PLUGINS LIKE post-templates-by-cat.php
 		// (WANTING TO BE LAST ISN'T GREEDY, SINCE I DON'T CHANGE ANYTHING, RIGHT?)
 		add_action('plugins_loaded', array("Template_Provisioning","add_template_filters"), 10);
+		
+		// ADD FILTER FOR STYLE TAG
+		add_filter('style_loader_tag', array("Template_Provisioning","filter_style_link_tags_for_less_js"), 10, 2);
 		
 		// ADD ACTIONS TO OUTPUT THE HEAD CONTENT
 		add_action('wp_head', array("Template_Provisioning","helpful_comment"));
@@ -93,6 +96,18 @@ class Template_Provisioning
 		return $template_filepath;
 	}
 	
+	function filter_style_link_tags_for_less_js($tag, $handle)
+	{
+  	global $wp_styles;
+  	
+  	// if the src ends in ".less", the rel attribute should be "stylesheet/less"
+  	if (preg_match("/\.less$/", $wp_styles->registered[$handle]->src)) {
+  	  $tag = preg_replace("/rel=(['\"])[^'\"]*(['\"])/", "rel=$1stylesheet/less$2", $tag);
+  	}
+  	
+	  return $tag;
+	}
+	
 	function helpful_comment()
 	{
 		$template_basename = Template_Provisioning::$template_basename;
@@ -107,46 +122,95 @@ class Template_Provisioning
 	{
 		global $is_IE;
 		
+		$extensions = array('css','less');
+		
 		$stylesheets = array();
-		$stylesheets[] = 'global.css';
-		if ($is_IE) $stylesheets[] = 'ie/global.css';
-		$stylesheets[] = Template_Provisioning::$template_basename.'.css';
-		if ($is_IE) $stylesheets[] = 'ie/'.Template_Provisioning::$template_basename.'.css';
+		$stylesheets[] = 'global';
+		if ($is_IE) $stylesheets[] = 'ie/global';
+		$stylesheets[] = Template_Provisioning::$template_basename;
+		if ($is_IE) $stylesheets[] = 'ie/'.Template_Provisioning::$template_basename;
 		
 		foreach($stylesheets as $stylesheet) {
-			if (file_exists(TEMPLATEPATH.'/css/'.$stylesheet)) {
-				wp_enqueue_style(
-					$handle = $stylesheet, 
-					$src = get_bloginfo('template_directory').'/css/'.$stylesheet,
-					$dependencies = array(),
-					$version = false,
-					$media = false
+		  foreach($extensions as $extension) {
+  		  $file_path = TEMPLATEPATH.'/css/'.$stylesheet.'.'.$extension;
+  			if (file_exists($file_path)) {
+          
+          // if .less file, be sure to enqueue less.js
+          if ($extension == 'less') Template_Provisioning::enqueue_js('less');
+          
+  				// get dependencies
+  				$dependencies = Template_Provisioning::get_dependencies($file_path);
+
+  				// prevent loading of admin styles
+  				wp_deregister_style($stylesheet.'.'.$extension);
+
+  				wp_enqueue_style(
+  					$handle = $stylesheet.'.'.$extension, 
+  					$src = get_bloginfo('template_directory').'/css/'.$stylesheet.'.'.$extension,
+  					$dependencies = $dependencies,
+  					$version = filemtime($file_path),
+  					$media = false
+  				);
+  			}
+		  }
+		}
+	}
+	
+	function enqueue_js($scripts = '')
+	{
+	  if (!$scripts = explode(',', $scripts)) {
+  		$scripts = array(
+  			'global',
+  			'global.footer',
+  			Template_Provisioning::$template_basename,
+  			Template_Provisioning::$template_basename.'.footer'
+  		);
+	  }
+		foreach($scripts as $script) {
+			$file_path = TEMPLATEPATH.'/js/'.$script.'.js';
+			if (file_exists($file_path)) {
+				
+				// get dependencies
+				$dependencies = Template_Provisioning::get_dependencies($file_path);
+				
+				// prevent loading of admin styles
+				wp_deregister_script($script);
+				
+				wp_enqueue_script(
+					$handle = $script,
+					$src = get_bloginfo('template_directory').'/js/'.$script.'.js',
+					$dependencies = $dependencies,
+					$version = filemtime($file_path),
+					$in_footer = (int) preg_match('/\.footer$/', $script)
 				);
 			}
 		}
 	}
 	
-	function enqueue_js()
+	function enqueue_less_js()
 	{
-		$scripts = array(
-			'global.js',
-			'global.footer.js',
-			Template_Provisioning::$template_basename.'.js',
-			Template_Provisioning::$template_basename.'.footer.js'
-		);
-		foreach($scripts as $script) {
-			if (file_exists(TEMPLATEPATH.'/js/'.$script)) {
-				wp_enqueue_script(
-					$handle = $script,
-					$src = get_bloginfo('template_directory').'/js/'.$script,
-					$dependencies = array(),
-					$version = false,
-					$in_footer = (int) preg_match('/\.footer\.js$/', $script)
-				);
-			}
-		}
+	  
 	}
-
+	
+	function get_dependencies($file_path)
+	{
+		$dependencies = array();
+		$file_contents = file($file_path);
+		$dep_lines = array_filter($file_contents, array("Template_Provisioning","is_dependency"));
+		foreach ($dep_lines as &$dep_line) {
+			$dep_line = trim(preg_replace('/^\/\/ ?NEEDS:/', '', $dep_line));
+			$dependencies = array_merge($dependencies, explode(',', $dep_line));
+		}
+		array_walk($dependencies, 'trim');
+		return $dependencies;
+	}
+	
+	function is_dependency($line)
+	{
+		// return true if line begins with // NEEDS:
+		return preg_match('/^\/\/ ?NEEDS:/', $line);
+	}
+	
 }
 
 /* --------------------------------------------------
